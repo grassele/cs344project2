ecccfc051eabc6e11dc3538a161f8f68341933a6
grassele <91402260+grassele@users.noreply.github.com>
2023-01-25 00:18:03 -0800

    deleted ugly comments

--- mycat.c
+++ mycat.c
@@ -16,20 +16,3 @@ int main(int argc, char *argv[]) {
         close(fd);
     }
 }
-
-
-// int main(int argc, char *argv[]) {
-
-//     for (int i = 1; i <= argc; i++) {
-//         int fd = open(argv[i], O_RDWR);
-
-//         int sz = read(fd, &argv[i], 2048);
-
-//         for (int j = 0; sz > 0; j++) {
-//             sz = read(fd, &argv[i]+2048*j, 2048);
-//             write(1, &argv[1], sz);
-//         }
-
-//         close(fd);
-//     }
-// }
